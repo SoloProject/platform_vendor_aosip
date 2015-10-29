2e2cdcfa29c0b462ae9f2bb58e206f8fb7f8a0d5
mike <mikew8978@gmail.com>
2015-10-28 23:12:20 -0400

    Update for marshmallow

--- products/common_full.mk
+++ products/common_full.mk
@@ -1,19 +1,3 @@
-# Copy prebuilt files.
-ifneq ($(filter aosip_hammerhead,$(TARGET_PRODUCT)),)
-PRODUCT_COPY_FILES += \
-    vendor/aosip/prebuilt/common/bootanimations/BOOTANIMATION-1280x768.zip:system/media/bootanimation.zip \
-        vendor/aosip/prebuilt/common/app/OmaDmclient.apk:system/app/OmaDmclient.apk
-endif
-
-ifneq ($(filter aosip_mako aosip_grouper aosip_flo,$(TARGET_PRODUCT)),)
-PRODUCT_COPY_FILES += \
-    vendor/aosip/prebuilt/common/bootanimations/BOOTANIMATION-768x480.zip:system/media/bootanimation.zip
-endif
-
-ifneq ($(filter aosip_shamu aosip_manta,$(TARGET_PRODUCT)),)
-PRODUCT_COPY_FILES += \
-    vendor/aosip/prebuilt/common/bootanimations/BOOTANIMATION-1440x2560.zip:system/media/bootanimation.zip
-endif
 
 # Apns
 ifneq ($(filter aosip_shamu aosip_hammerhead aosip_mako,$(TARGET_PRODUCT)),)
@@ -37,12 +21,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_COPY_FILES += \
     vendor/aosip/prebuilt/common/etc/UPDATE-SuperSU.zip:system/addon.d/UPDATE-SuperSU.zip \
     vendor/aosip/prebuilt/common/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon \
-
-# Google apps
-PRODUCT_COPY_FILES += \
-    vendor/aosip/overlay/common/packages/apps/Messenger.apk:system/priv-app/Messenger.apk \
-    vendor/aosip/overlay/common/packages/apps/Launcher.apk:system/app/Google_Launcher.apk \
-    vendor/aosip/overlay/common/packages/apps/Callendar.apk:system/app/Google_Callendar.apk
 
 # Inherit common product build prop overrides
 -include vendor/aosip/products/common_versions.mk
